4edf384394737a1785f553ddf21b1c6d036852d8
z <1030453588@qq.com>
2020-09-26 20:27:36 +0800

    test

---
+++ src/main/java/com/example/springbootmybatisplusexample/SpringbootMybatisPlusExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmybatisplusexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisPlusExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisPlusExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmybatisplusexample/SpringbootMybatisPlusExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmybatisplusexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPlusExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
